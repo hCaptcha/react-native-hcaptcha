a8a3d5ef0aeaae8ee02c36d9a4a7812ce36126cb
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-09-23 17:13:55 +0530

    (refactor) added custom props to support loadingIndicator, background colours

--- index.js
+++ index.js
@@ -1,62 +1,96 @@
-import React, { Component } from 'react';
-import {
-    SafeAreaView, StyleSheet, Dimensions
-} from 'react-native';
-import Modal from 'react-native-modal';
-import Hcaptcha from './Hcaptcha';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from "react";
+import { SafeAreaView, StyleSheet, Dimensions } from "react-native";
+import Modal from "react-native-modal";
+import Hcaptcha from "./Hcaptcha";
+import PropTypes from "prop-types";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
-class ConfirmHcaptcha extends Component {
-    state = {
-        show: false
-    }
-    show = () => {
-        this.setState({ show: true });
-    }
-    hide = () => {
-        this.setState({ show: false });
-    }
-    render() {
-        let { show } = this.state;
-        let { siteKey, baseUrl, languageCode, onMessage, cancelButtonText } = this.props;
-        return (
-            <Modal
-                useNativeDriver
-                hideModalContentWhileAnimating
-                deviceHeight={height}
-                deviceWidth={width}
-                style={styles.modal}
-                animationIn="fadeIn"
-                animationOut='fadeOut'
-                onBackdropPress={this.hide}
-                onBackButtonPress={this.hide}
-                isVisible={show}>
-                <SafeAreaView style={styles.wrapper}>
-                    <Hcaptcha
-                        url={baseUrl}
-                        siteKey={siteKey}
-                        onMessage={onMessage}
-                        languageCode={languageCode}
-                        cancelButtonText={cancelButtonText}
-                    />
-                </SafeAreaView>
-            </Modal>
-        );
-    }
+class ConfirmHcaptcha extends PureComponent {
+  state = {
+    show: false,
+  };
+  show = () => {
+    this.setState({ show: true });
+  };
+  hide = () => {
+    const { onMessage } = this.props;
+    this.setState({ show: false });
+    onMessage({ nativeEvent: { data: "cancel" } });
+  };
+  render() {
+    let { show } = this.state;
+    let {
+      siteKey,
+      baseUrl,
+      languageCode,
+      onMessage,
+      cancelButtonText,
+      showLoading,
+      backgroundColor,
+      loadingIndicatorColor,
+    } = this.props;
+    return (
+      <Modal
+        useNativeDriver
+        hideModalContentWhileAnimating
+        deviceHeight={height}
+        deviceWidth={width}
+        style={styles.modal}
+        animationIn="fadeIn"
+        animationOut="fadeOut"
+        onBackdropPress={this.hide}
+        onBackButtonPress={this.hide}
+        isVisible={show}
+      >
+        <SafeAreaView style={[styles.wrapper, { backgroundColor }]}>
+          <Hcaptcha
+            url={baseUrl}
+            siteKey={siteKey}
+            onMessage={onMessage}
+            languageCode={languageCode}
+            cancelButtonText={cancelButtonText}
+            showLoading={showLoading}
+            loadingIndicatorColor={loadingIndicatorColor}
+            backgroundColor={backgroundColor}
+          />
+        </SafeAreaView>
+      </Modal>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    text: { fontSize: 15, fontWeight: 'bold', color: '#fff', textAlign: 'center', marginTop: 10 },
-    modal: { margin: 0 },
-    wrapper: { flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.3)', justifyContent: 'center', overflow: 'hidden' }
+  text: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+    marginTop: 10,
+  },
+  modal: { margin: 0 },
+  wrapper: {
+    flex: 1,
+    justifyContent: "center",
+    overflow: "hidden",
+  },
 });
+
 ConfirmHcaptcha.propTypes = {
-    siteKey: PropTypes.string.isRequired,
-    baseUrl: PropTypes.string,
-    onMessage: PropTypes.func,
-    languageCode: PropTypes.string,
-    cancelButtonText: PropTypes.string
-}
+  siteKey: PropTypes.string.isRequired,
+  baseUrl: PropTypes.string,
+  onMessage: PropTypes.func,
+  languageCode: PropTypes.string,
+  cancelButtonText: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  showLoading: PropTypes.bool,
+  loadingIndicatorColor: PropTypes.string,
+};
+
+ConfirmHcaptcha.defaultProps = {
+  showLoading: false,
+  backgroundColor: "rgba(0, 0, 0, 0.3)",
+  loadingIndicatorColor: null,
+};
+
 export default ConfirmHcaptcha;
