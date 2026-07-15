8484367075ad3df46909b7cec6bbb0c080c31690
Damian Nunez Rodriguez <damian.rodriguez@intuitionmachines.com>
2026-07-15 10:11:05 -0300

    fix: support React Native 0.86 Jest preset

--- Example.jest.setup.js
+++ Example.jest.setup.js
@@ -1,8 +1,11 @@
 import { jest } from '@jest/globals';
 
+jest.mock('@hcaptcha/loader/inline', () => (
+  'window.hCaptchaLoader = function(config) { window.hCaptchaLoaderConfig = config; return Promise.resolve(window.hcaptcha); };'
+), { virtual: true });
+
 jest.mock('react-native-webview', () => {
   return {
     WebView: () => 'WebView',
   };
 });
-
