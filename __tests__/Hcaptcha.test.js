30f26bd20e014c951f8ca1cf949aa319f1ccfe2b
Damian Nunez Rodriguez <damian.rodriguez@intuitionmachines.com>
2026-07-15 17:31:07 -0300

    chore: bump version to 4.1.0

--- __tests__/Hcaptcha.test.js
+++ __tests__/Hcaptcha.test.js
@@ -103,7 +103,7 @@ describe('Hcaptcha', () => {
     expect(config.debugInfo).toMatchObject({
       customDebug: 'enabled',
       'dep_mocked-md5': true,
-      sdk_4_0_0: true,
+      sdk_4_1_0: true,
     });
 
     expect(loaderConfig).toMatchObject({
